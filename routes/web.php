9ab3e6e9db1125bcf6865c13d5571ebeeeeec2e2
Nks <niko.afv@gmail.com>
2018-01-18 02:23:56 -0300

    web route updated

--- routes/web.php
+++ routes/web.php
@@ -88,6 +88,6 @@ Route::get('/facebook/callback', function(SammyK\LaravelFacebookSdk\LaravelFaceb
     Auth::user()->save();
 
     //return redirect('/api/'.Auth::user()->id.'/'.$token);
-    $return_url = env('CLIENT_APP_URL'). '/'.Auth::user()->id.'/'.$token->getValue();
+    $return_url = env('CLIENT_APP_URL'). '/'.Auth::user()->id.'/'.$token;
     return redirect($return_url);
 });
